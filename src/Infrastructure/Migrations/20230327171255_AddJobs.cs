55be6ca26208469656987f26ad5897bbc7a42689
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-04-29 16:18:44 -0300

    fix(migrations): fix namespaces :bug:

--- src/Infrastructure/Migrations/20230327171255_AddJobs.cs
+++ src/Infrastructure/Migrations/20230327171255_AddJobs.cs
@@ -1,131 +1,130 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Infrastructure.Migrations
 {
+  /// <inheritdoc />
+  public partial class AddJobs : Migration
+  {
     /// <inheritdoc />
-    public partial class AddJobs : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "authors",
-                columns: table => new
-                {
-                    id = table.Column<string>(type: "text", nullable: false),
-                    name = table.Column<string>(type: "text", nullable: true),
-                    avatar_url = table.Column<string>(type: "text", nullable: true),
-                    createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_authors", x => x.id);
-                });
+      migrationBuilder.CreateTable(
+          name: "authors",
+          columns: table => new
+          {
+            id = table.Column<string>(type: "text", nullable: false),
+            name = table.Column<string>(type: "text", nullable: true),
+            avatar_url = table.Column<string>(type: "text", nullable: true),
+            createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+            updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_authors", x => x.id);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "announcements",
-                columns: table => new
-                {
-                    id = table.Column<string>(type: "text", nullable: false),
-                    title = table.Column<string>(type: "text", nullable: true),
-                    description = table.Column<string>(type: "text", nullable: true),
-                    tags = table.Column<string>(type: "text", nullable: true),
-                    url = table.Column<string>(type: "text", nullable: true),
-                    authorid = table.Column<string>(type: "text", nullable: true),
-                    createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_announcements", x => x.id);
-                    table.ForeignKey(
-                        name: "FK_announcements_authors_authorid",
-                        column: x => x.authorid,
-                        principalTable: "authors",
-                        principalColumn: "id");
-                });
+      migrationBuilder.CreateTable(
+          name: "announcements",
+          columns: table => new
+          {
+            id = table.Column<string>(type: "text", nullable: false),
+            title = table.Column<string>(type: "text", nullable: true),
+            description = table.Column<string>(type: "text", nullable: true),
+            tags = table.Column<string>(type: "text", nullable: true),
+            url = table.Column<string>(type: "text", nullable: true),
+            authorid = table.Column<string>(type: "text", nullable: true),
+            createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+            updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_announcements", x => x.id);
+            table.ForeignKey(
+                      name: "FK_announcements_authors_authorid",
+                      column: x => x.authorid,
+                      principalTable: "authors",
+                      principalColumn: "id");
+          });
 
-            migrationBuilder.CreateTable(
-                name: "proposals",
-                columns: table => new
-                {
-                    id = table.Column<string>(type: "text", nullable: false),
-                    price = table.Column<int>(type: "integer", nullable: false),
-                    description = table.Column<string>(type: "text", nullable: true),
-                    announcementid = table.Column<string>(type: "text", nullable: true),
-                    createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_proposals", x => x.id);
-                    table.ForeignKey(
-                        name: "FK_proposals_announcements_announcementid",
-                        column: x => x.announcementid,
-                        principalTable: "announcements",
-                        principalColumn: "id");
-                });
+      migrationBuilder.CreateTable(
+          name: "proposals",
+          columns: table => new
+          {
+            id = table.Column<string>(type: "text", nullable: false),
+            price = table.Column<int>(type: "integer", nullable: false),
+            description = table.Column<string>(type: "text", nullable: true),
+            announcementid = table.Column<string>(type: "text", nullable: true),
+            createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+            updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_proposals", x => x.id);
+            table.ForeignKey(
+                      name: "FK_proposals_announcements_announcementid",
+                      column: x => x.announcementid,
+                      principalTable: "announcements",
+                      principalColumn: "id");
+          });
 
-            migrationBuilder.CreateTable(
-                name: "jobs",
-                columns: table => new
-                {
-                    id = table.Column<string>(type: "text", nullable: false),
-                    status = table.Column<int>(type: "integer", nullable: false),
-                    proposalid = table.Column<string>(type: "text", nullable: true),
-                    createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_jobs", x => x.id);
-                    table.ForeignKey(
-                        name: "FK_jobs_proposals_proposalid",
-                        column: x => x.proposalid,
-                        principalTable: "proposals",
-                        principalColumn: "id");
-                });
+      migrationBuilder.CreateTable(
+          name: "jobs",
+          columns: table => new
+          {
+            id = table.Column<string>(type: "text", nullable: false),
+            status = table.Column<int>(type: "integer", nullable: false),
+            proposalid = table.Column<string>(type: "text", nullable: true),
+            createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+            updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_jobs", x => x.id);
+            table.ForeignKey(
+                      name: "FK_jobs_proposals_proposalid",
+                      column: x => x.proposalid,
+                      principalTable: "proposals",
+                      principalColumn: "id");
+          });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_announcements_authorid",
-                table: "announcements",
-                column: "authorid");
+      migrationBuilder.CreateIndex(
+          name: "IX_announcements_authorid",
+          table: "announcements",
+          column: "authorid");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_announcements_url",
-                table: "announcements",
-                column: "url",
-                unique: true);
+      migrationBuilder.CreateIndex(
+          name: "IX_announcements_url",
+          table: "announcements",
+          column: "url",
+          unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_jobs_proposalid",
-                table: "jobs",
-                column: "proposalid");
+      migrationBuilder.CreateIndex(
+          name: "IX_jobs_proposalid",
+          table: "jobs",
+          column: "proposalid");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_proposals_announcementid",
-                table: "proposals",
-                column: "announcementid");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "jobs");
-
-            migrationBuilder.DropTable(
-                name: "proposals");
-
-            migrationBuilder.DropTable(
-                name: "announcements");
-
-            migrationBuilder.DropTable(
-                name: "authors");
-        }
+      migrationBuilder.CreateIndex(
+          name: "IX_proposals_announcementid",
+          table: "proposals",
+          column: "announcementid");
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "jobs");
+
+      migrationBuilder.DropTable(
+          name: "proposals");
+
+      migrationBuilder.DropTable(
+          name: "announcements");
+
+      migrationBuilder.DropTable(
+          name: "authors");
+    }
+  }
 }
--- src/Infrastructure/Migrations/20230429040151_AddPlatforms.Designer.cs
+++ src/Infrastructure/Migrations/20230429040151_AddPlatforms.Designer.cs
@@ -11,231 +11,231 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Infrastructure.Migrations
 {
-    [DbContext(typeof(PersistenceDatabaseContext))]
-    [Migration("20230429040151_AddPlatforms")]
-    partial class AddPlatforms
+  [DbContext(typeof(PersistenceDatabaseContext))]
+  [Migration("20230429040151_AddPlatforms")]
+  partial class AddPlatforms
+  {
+    /// <inheritdoc />
+    protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.4")
-                .HasAnnotation("Relational:MaxIdentifierLength", 63);
+      modelBuilder
+          .HasAnnotation("ProductVersion", "7.0.4")
+          .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
-            NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
+      NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Announcement", b =>
-                {
-                    b.Property<string>("id")
-                        .HasColumnType("text");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Announcements.Announcement", b =>
+          {
+            b.Property<string>("id")
+                      .HasColumnType("text");
 
-                    b.Property<string>("authorid")
-                        .HasColumnType("text");
+            b.Property<string>("authorid")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("createdAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("createdAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("description")
-                        .HasColumnType("text");
+            b.Property<string>("description")
+                      .HasColumnType("text");
 
-                    b.Property<string>("platformid")
-                        .HasColumnType("text");
+            b.Property<string>("platformid")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("publishedAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("publishedAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<int>("status")
-                        .HasColumnType("integer");
+            b.Property<int>("status")
+                      .HasColumnType("integer");
 
-                    b.Property<string>("tags")
-                        .HasColumnType("text");
+            b.Property<string>("tags")
+                      .HasColumnType("text");
 
-                    b.Property<string>("title")
-                        .HasColumnType("text");
+            b.Property<string>("title")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("updatedAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("updatedAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("url")
-                        .HasColumnType("text");
+            b.Property<string>("url")
+                      .HasColumnType("text");
 
-                    b.HasKey("id");
+            b.HasKey("id");
 
-                    b.HasIndex("authorid");
+            b.HasIndex("authorid");
 
-                    b.HasIndex("platformid");
+            b.HasIndex("platformid");
 
-                    b.HasIndex("url")
-                        .IsUnique();
+            b.HasIndex("url")
+                      .IsUnique();
 
-                    b.ToTable("announcements");
-                });
+            b.ToTable("announcements");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Author", b =>
-                {
-                    b.Property<string>("id")
-                        .HasColumnType("text");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Announcements.Author", b =>
+          {
+            b.Property<string>("id")
+                      .HasColumnType("text");
 
-                    b.Property<string>("avatar_url")
-                        .HasColumnType("text");
+            b.Property<string>("avatar_url")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("createdAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("createdAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("name")
-                        .HasColumnType("text");
+            b.Property<string>("name")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("updatedAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("updatedAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.HasKey("id");
+            b.HasKey("id");
 
-                    b.ToTable("authors");
-                });
+            b.ToTable("authors");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Job", b =>
-                {
-                    b.Property<string>("id")
-                        .HasColumnType("text");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Job", b =>
+          {
+            b.Property<string>("id")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("createdAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("createdAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("proposalid")
-                        .HasColumnType("text");
+            b.Property<string>("proposalid")
+                      .HasColumnType("text");
 
-                    b.Property<int>("status")
-                        .HasColumnType("integer");
+            b.Property<int>("status")
+                      .HasColumnType("integer");
 
-                    b.Property<DateTime>("updatedAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("updatedAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.HasKey("id");
+            b.HasKey("id");
 
-                    b.HasIndex("proposalid");
+            b.HasIndex("proposalid");
 
-                    b.ToTable("jobs");
-                });
+            b.ToTable("jobs");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Proposal", b =>
-                {
-                    b.Property<string>("id")
-                        .HasColumnType("text");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Proposals.Proposal", b =>
+          {
+            b.Property<string>("id")
+                      .HasColumnType("text");
 
-                    b.Property<string>("announcementid")
-                        .HasColumnType("text");
+            b.Property<string>("announcementid")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("createdAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("createdAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("description")
-                        .HasColumnType("text");
+            b.Property<string>("description")
+                      .HasColumnType("text");
 
-                    b.Property<int>("price")
-                        .HasColumnType("integer");
+            b.Property<int>("price")
+                      .HasColumnType("integer");
 
-                    b.Property<DateTime>("updatedAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("updatedAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.HasKey("id");
+            b.HasKey("id");
 
-                    b.HasIndex("announcementid");
+            b.HasIndex("announcementid");
 
-                    b.ToTable("proposals");
-                });
+            b.ToTable("proposals");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Platforms.Platform", b =>
-                {
-                    b.Property<string>("id")
-                        .HasColumnType("text");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Platforms.Platform", b =>
+          {
+            b.Property<string>("id")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("createdAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("createdAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("icon_url")
-                        .HasColumnType("text");
+            b.Property<string>("icon_url")
+                      .HasColumnType("text");
 
-                    b.Property<string>("name")
-                        .HasColumnType("text");
+            b.Property<string>("name")
+                      .HasColumnType("text");
 
-                    b.Property<string>("thumb_url")
-                        .HasColumnType("text");
+            b.Property<string>("thumb_url")
+                      .HasColumnType("text");
 
-                    b.Property<DateTime>("updatedAt")
-                        .HasColumnType("timestamp with time zone");
+            b.Property<DateTime>("updatedAt")
+                      .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("url")
-                        .HasColumnType("text");
+            b.Property<string>("url")
+                      .HasColumnType("text");
 
-                    b.HasKey("id");
+            b.HasKey("id");
 
-                    b.HasIndex("url")
-                        .IsUnique();
+            b.HasIndex("url")
+                      .IsUnique();
 
-                    b.ToTable("platforms");
+            b.ToTable("platforms");
 
-                    b.HasData(
-                        new
-                        {
-                            id = "162b482f-a5c9-4ce4-8921-bc0b39eb1fe9",
-                            createdAt = new DateTime(2023, 4, 29, 4, 1, 51, 564, DateTimeKind.Utc).AddTicks(6448),
-                            icon_url = "https://icones.pro/wp-content/uploads/2021/03/symbole-de-plume-bleue.png",
-                            name = "freelancerbr",
-                            thumb_url = "https://freelancer.com.br/images/main-page/find-section-bg.jpg",
-                            updatedAt = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            url = "https://freelancer.com.br"
-                        },
-                        new
-                        {
-                            id = "69f1f410-a320-417b-9d44-762ac5970810",
-                            createdAt = new DateTime(2023, 4, 29, 4, 1, 51, 564, DateTimeKind.Utc).AddTicks(6467),
-                            icon_url = "https://play-lh.googleusercontent.com/yNBbWLb1_Te7UELDjx4OjKpkYcW79nklxeB-xJThgaN0hiXpIDs2hHt3P9XISi4fEjwH=w240-h480-rw",
-                            name = "99freelas",
-                            thumb_url = "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2014%2F04%2F07%2F16%2FWDL-Logo-39095_16236_061350792_1651536830.jpg",
-                            updatedAt = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            url = "https://www.99freelas.com.br"
-                        });
-                });
+            b.HasData(
+                      new
+                      {
+                        id = "162b482f-a5c9-4ce4-8921-bc0b39eb1fe9",
+                        createdAt = new DateTime(2023, 4, 29, 4, 1, 51, 564, DateTimeKind.Utc).AddTicks(6448),
+                        icon_url = "https://icones.pro/wp-content/uploads/2021/03/symbole-de-plume-bleue.png",
+                        name = "freelancerbr",
+                        thumb_url = "https://freelancer.com.br/images/main-page/find-section-bg.jpg",
+                        updatedAt = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                        url = "https://freelancer.com.br"
+                      },
+                      new
+                      {
+                        id = "69f1f410-a320-417b-9d44-762ac5970810",
+                        createdAt = new DateTime(2023, 4, 29, 4, 1, 51, 564, DateTimeKind.Utc).AddTicks(6467),
+                        icon_url = "https://play-lh.googleusercontent.com/yNBbWLb1_Te7UELDjx4OjKpkYcW79nklxeB-xJThgaN0hiXpIDs2hHt3P9XISi4fEjwH=w240-h480-rw",
+                        name = "99freelas",
+                        thumb_url = "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2014%2F04%2F07%2F16%2FWDL-Logo-39095_16236_061350792_1651536830.jpg",
+                        updatedAt = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                        url = "https://www.99freelas.com.br"
+                      });
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Announcement", b =>
-                {
-                    b.HasOne("AnyTime.Core.Domain.Modules.Jobs.Author", "author")
-                        .WithMany("announcements")
-                        .HasForeignKey("authorid");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Announcements.Announcement", b =>
+          {
+            b.HasOne("AnyTime.Core.Domain.Modules.Announcements.Author", "author")
+                      .WithMany("announcements")
+                      .HasForeignKey("authorid");
 
-                    b.HasOne("AnyTime.Core.Domain.Modules.Platforms.Platform", "platform")
-                        .WithMany()
-                        .HasForeignKey("platformid");
+            b.HasOne("AnyTime.Core.Domain.Modules.Platforms.Platform", "platform")
+                      .WithMany()
+                      .HasForeignKey("platformid");
 
-                    b.Navigation("author");
+            b.Navigation("author");
 
-                    b.Navigation("platform");
-                });
+            b.Navigation("platform");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Job", b =>
-                {
-                    b.HasOne("AnyTime.Core.Domain.Modules.Jobs.Proposal", "proposal")
-                        .WithMany()
-                        .HasForeignKey("proposalid");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Job", b =>
+          {
+            b.HasOne("AnyTime.Core.Domain.Modules.Proposals.Proposal", "proposal")
+                      .WithMany()
+                      .HasForeignKey("proposalid");
 
-                    b.Navigation("proposal");
-                });
+            b.Navigation("proposal");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Proposal", b =>
-                {
-                    b.HasOne("AnyTime.Core.Domain.Modules.Jobs.Announcement", "announcement")
-                        .WithMany()
-                        .HasForeignKey("announcementid");
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Proposals.Proposal", b =>
+          {
+            b.HasOne("AnyTime.Core.Domain.Modules.Announcements.Announcement", "announcement")
+                      .WithMany()
+                      .HasForeignKey("announcementid");
 
-                    b.Navigation("announcement");
-                });
+            b.Navigation("announcement");
+          });
 
-            modelBuilder.Entity("AnyTime.Core.Domain.Modules.Jobs.Author", b =>
-                {
-                    b.Navigation("announcements");
-                });
+      modelBuilder.Entity("AnyTime.Core.Domain.Modules.Announcements.Author", b =>
+          {
+            b.Navigation("announcements");
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
