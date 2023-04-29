14bb5221216f679fa77620fc67e073d3c688a759
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-04-28 23:11:28 -0300

    fix(CLI/job): fix command flow by handlers :bug:

--- src/CLI/Commands/Jobs/Jobs.cs
+++ src/CLI/Commands/Jobs/Jobs.cs
@@ -6,6 +6,7 @@ namespace AnyTime.CLI.Commands.Jobs;
 
 using AnyTime.CLI.Commands.Jobs.SubCommands;
 using AnyTime.CLI.Commands.Shared;
+using AnyTime.Core.Application.Features.Announcements.Queries.FilterAnnouncements;
 using AnyTime.Core.Application.Features.Announcements.Queries.ScrapAnnouncements;
 using AnyTime.Core.Application.Features.Jobs.Commands.CreateJob;
 using AnyTime.Core.Application.Features.Proposals.Commands.CreateProposal;
@@ -23,22 +24,18 @@ public class JobsCommand : BaseCommand
 
   private static string GetVersion() => typeof(AnyTimeCommand).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
-
-
-
-
   protected async override Task<int> OnExecute(CommandLineApplication app)
   {
-    var announcements = await _mediator.Send<List<Announcement>>(new Scrap99FreelasAnnouncementsQuery { headless = true });
+    var announcements = await _mediator.Send<IReadOnlyList<Announcement>>(new ScrapFreelancerBrAnnouncementsQuery { headless = false });
 
-    var proposals = await _mediator.Send<IReadOnlyList<Proposal>>(new CreateProposalsCommand { announcements = announcements, platform = "99freelas", proposal_template = "CommonProposal.md" });
+    var filteredAnnouncements = await _mediator.Send<IReadOnlyList<Announcement>>(new FilterAnnouncementsListWithNLPQuery { announcements = announcements });
+
+    var proposals = await _mediator.Send<IReadOnlyList<Proposal>>(new CreateProposalsCommand { announcements = filteredAnnouncements, proposal_template = "CommonProposal.md" });
 
     await _mediator.Send<Unit>(new CreateJobsByProposalsCommand { proposals = proposals });
 
-    Console.WriteLine($"From {announcements.Count} announcements you can apply to ${proposals.Count}");
+    Console.WriteLine($"From {announcements.Count} scrapped ${filteredAnnouncements.Count} are valid announcements");
 
     return await base.OnExecute(app);
   }
-
-
 }
