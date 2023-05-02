af59aa26b0d33605940e7fc6298603634ace601e
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-05-02 01:48:30 -0300

    feat(domain/users): add user entity :sparkles:

---
+++ src/Core/Domain/Users/User.cs
@@ -0,0 +1,12 @@
+
+using AnyTime.Core.Domain.Shared;
+
+namespace AnyTime.Core.Domain.Modules.Users;
+public class User : BaseEntity
+{
+  public string first_name { get; set; }
+  public string last_name { get; set; }
+  public string username { get; set; }
+  public string email { get; set; }
+  public string password { get; set; }
+}
